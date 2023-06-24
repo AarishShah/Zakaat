481d5b0362d2bc6a86df76775bee1491a5219101
Alpha <syedaarishshah@gmail.com>
2023-06-24 18:17:20 +0530

    added comments

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Website we are collecting the data from: https://www.goodreturns.in/gold-rates/united-states.html
 // Documenttaion for table-scraper: https://www.npmjs.com/package/table-scraper
+// reference (ignore): https://www.npmjs.com/package/live-fuel-currency-gold-prices
 
 var scraper = require('table-scraper');
 const location = 'united-states'; // user will enter united states, change that ti united-states @KhushbooHamid
