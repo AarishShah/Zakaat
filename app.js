98fca044a5bc6a7aab8cba9cd4b721a21eb73232
Alpha <syedaarishshah@gmail.com>
2023-06-24 18:27:27 +0530

    added steps

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// Website we are collecting the data from: https://www.goodreturns.in/gold-rates/united-states.html
+// Website-1 we are collecting the data from: https://www.goodreturns.in/gold-rates/united-states.html
+// Website-2 (we will implement this later): https://pricegold.net/today/
 // Documenttaion for table-scraper: https://www.npmjs.com/package/table-scraper
 // reference (ignore): https://www.npmjs.com/package/live-fuel-currency-gold-prices
 
@@ -11,4 +12,9 @@ scraper
     .then(function (tableData)
     {
         console.log(tableData);
+        /*
+        the output will be an array of arrays.'
+        next step is to post relevant data only
+            relevant data is the first two table on the website 1 (22k and 24k)
+        */
     });
