495fa37999f43306dfd424e91fe8ba3051256e00
Alpha <syedaarishshah@gmail.com>
2023-06-24 21:23:56 +0530

    refactored

--- app.js
+++ app.js
@@ -12,19 +12,17 @@ scraper
     .then(function (tableData)
     {
         // console.log(tableData); // to display entire list of tables
-        const weigthofcarat22 = tableData[0][1][0]; // 1 gram gold
-        const carat22 = tableData[0][0][1]; // 22 carat gold        
-        const stringCostofcarat22 = tableData[0][1][1]; // cost of 1 gram of 22 carat gold
-
-        // applying regex to get the number only
-        const stringWithCurrency = stringCostofcarat22;
-        const costofcarat22 = parseFloat(stringWithCurrency.replace(/[^0-9.-]+/g, "")); // regex to get the number only
-
-        // applying regex to get the currency symbol only
-        const stringWithNumbers = stringCostofcarat22;
-        const currencySymbol = stringWithNumbers.replace(/[0-9.,]+/g, "");
-
-        console.log("The cost of " + weigthofcarat22 + " of " + carat22 + " gold is: " + costofcarat22 + " " + currencySymbol);
+        const weightOfCarat22 = tableData[0][1][0]; // 1 gram gold
+        const carat22 = tableData[0][0][1]; // 22 carat gold
+        const stringCostOfCarat22 = tableData[0][1][1]; // cost of 1 gram of 22 carat gold
+      
+        // Extract the number only using regex
+        const costOfCarat22 = parseFloat(stringCostOfCarat22.replace(/[^0-9.-]+/g, ''));
+      
+        // Extract the currency symbol only using regex
+        const currencySymbol = stringCostOfCarat22.replace(/[0-9.,]+/g, '');
+      
+        console.log(`The cost of ${weightOfCarat22} of ${carat22} gold is: ${costOfCarat22} ${currencySymbol}`);
 
         /*
         the output will be an array of arrays.'
