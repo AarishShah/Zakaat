948b5daf812e33bd647f95caf5d7c61e17f2e977
Alpha <syedaarishshah@gmail.com>
2023-06-24 18:13:16 +0530

    using table scraper
    Co-authored-by: Khushi <KhushbooHamid@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-const request = require('postman-request');
+var scraper = require('table-scraper');
+const location = 'united-states'; // user will enter united states, change that ti united-states @KhushbooHamid
+const url = `https://www.goodreturns.in/gold-rates/${location}.html`;
 
-const key = 'fc9d2ffbae683086a1ea46d9ac12e5aa' // 95 requests remaining
-const base = INR;
-const url = 'https://api.metalpriceapi.com/v1/latest?api_key=' + key + '&base=' + base;
-
-request
-    (
-        { url: url, json: true },
-
-        (error, response) =>
-        {
-            if (error) // means no response
-                console.log('Unable to connect to service!');
-
-            else console.log(response.body);
-
-        }
-    )
+scraper
+    .get(url)
+    .then(function (tableData)
+    {
+        console.log(tableData);
+    });
