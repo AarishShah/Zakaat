9a667c4034a2a642bd8ace12413dfe671dc0f0d0
Alpha <syedaarishshah@gmail.com>
2023-07-05 16:21:13 +0530

    added a commenrt in line 1

--- try.js
+++ try.js
@@ -1,5 +1,7 @@
+// Website-2 (we will implement this later): https://pricegold.net/today/ (we will use this for countries)
+
 var scraper = require('table-scraper');
- const location = 'us-united-states';
+const location = 'us-united-states';
 const url = `https://pricegold.net/${location}/24k-gold/`;
 
 
@@ -7,14 +9,14 @@ scraper
     .get(url)
     .then(function (tableData)
     {
-    console.log(tableData[0][0])//kilo ounce and gram price
-    console.log(tableData[0][1])//kilo ounce and gram price
-    console.log(tableData[0][2])//kilo ounce and gram price
+        console.log(tableData[0][0])//kilo ounce and gram price
+        console.log(tableData[0][1])//kilo ounce and gram price
+        console.log(tableData[0][2])//kilo ounce and gram price
 
 
-    console.log(tableData[2][0])//gold price in the selected country
+        console.log(tableData[2][0])//gold price in the selected country
 
-    // const weightOfCarat24=tableData[0];
-    // const priceInCountry=tableData[2][0];
+        // const weightOfCarat24=tableData[0];
+        // const priceInCountry=tableData[2][0];
 
     });
