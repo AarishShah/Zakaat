a80b99d6a3ae4fa77b7812dd1dde483acd2e0d67
khushi <khushboohamid786@gmail.com>
2023-07-05 16:15:44 +0530

    changes

---
+++ try.js
@@ -0,0 +1,20 @@
+var scraper = require('table-scraper');
+ const location = 'us-united-states';
+const url = `https://pricegold.net/${location}/24k-gold/`;
+
+
+scraper
+    .get(url)
+    .then(function (tableData)
+    {
+    console.log(tableData[0][0])//kilo ounce and gram price
+    console.log(tableData[0][1])//kilo ounce and gram price
+    console.log(tableData[0][2])//kilo ounce and gram price
+
+
+    console.log(tableData[2][0])//gold price in the selected country
+
+    // const weightOfCarat24=tableData[0];
+    // const priceInCountry=tableData[2][0];
+
+    });
