5475839d139c66d78fcd72c7df5ab87ea42cd5c3
Alpha <syedaarishshah@gmail.com>
2023-07-05 17:21:03 +0530

    remove redundant loc

--- try.js
+++ try.js
@@ -10,23 +10,17 @@ scraper
     .get(url)
     .then(function (tableData)
     {
-       //  console.log(tableData);
-    //    console.log(tableData[0]) // firsd table on the website
-        console.log(tableData[0][0])//kilo ounce and gram price
-         console.log(tableData[0][1])//kilo ounce and gram price
-         console.log(tableData[0][2])//kilo ounce and gram price
-         console.log(tableData[0][3])//kilo ounce and gram price
-         console.log(tableData[0][4])//kilo ounce and gram price
-         console.log(tableData[0][5])//kilo ounce and gram price
+        // console.log(tableData[0]) // firsd table on the website
+        const goldData = tableData[0];
+        console.log(goldData[0])// 24k 1Gram
+        console.log(goldData[1])// 22k 1Gram
+        console.log(goldData[2])// 21k 1Gram
+        console.log(goldData[3])// 18k 1Gram
+        console.log(goldData[4])// 14k 1Gram
+        console.log(goldData[5])// 10k 1Gram
 
 
-       // console.log(tableData[2][0])//gold price in the selected country
-
-//         const weightOfCarat24Gm=tableData[0][0];
-//         const weightOfCarat24Ounce=tableData[0][1];
-//         const weightOfCarat24Kg=tableData[0][2];
-//         const priceInCountry=tableData[2][0];
 
 
-//  console.log(`The cost of ${weightOfCarat24Gm} gm, ${weightOfCarat24Ounce} ounce and ${weightOfCarat24Kg}  kg gold is ${priceInCountry}`);
-   });
+        //  console.log(`The cost of ${weightOfCarat24Gm} gm, ${weightOfCarat24Ounce} ounce and ${weightOfCarat24Kg}  kg gold is ${priceInCountry}`);
+    });
