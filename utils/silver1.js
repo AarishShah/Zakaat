b2470d957cf010e354109636e74d53e9a1daf35b
khushi <khushboohamid786@gmail.com>
2023-07-25 19:49:04 +0530

    completed silver1(Indian states only)

--- utils/silver1.js
+++ utils/silver1.js
@@ -1,5 +1,4 @@
-// Data source: https://pricegold.net/today/ (we will use this for countries)
-// Documenttaion for table-scraper: https://www.npmjs.com/package/table-scraper
+// Data source: https://www.goodreturns.in/silver-rates/ahmedabad.html (we will use this for states in India only)
 
 var scraper = require('table-scraper');
 const location = 'ahmedabad';
@@ -8,18 +7,16 @@ const url = `https://www.goodreturns.in/silver-rates/${location}.html`;
 scraper
     .get(url)
     .then(function (tableData) {
-        // console.log(tableData[0]) // first table on the website
-        //   const goldData = tableData[0];
+        
+        const silverData = tableData[0][1];
+        const weight = silverData[0];
+        const value = silverData[1];
+        const currencySymbol = value.replace(/[0-9.,]+/g, '');
+        const cost = value.replace(/[^0-9.-]+/g, '')
 
-        //const sildata=tableData[0][1];
-        // const first2 = tableData[0][1].replace(/[^0-9.,]+/g, '');
-        // console.log(first2);
-        //console.log(tableData[0][1]);
-        console.log(tableData[1][1]);//nearest date
-        console.log(tableData[2][2]);//highest rate in last month
-
-        // const nearestDate=tableData[1][1];
-        // const highestRate=tableData[2][2];
-
+        console.log(weight);
+        console.log(currencySymbol);
+        console.log(cost);
+        console.log("\n");
 
     });
