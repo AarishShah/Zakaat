3885753156e911ec3e459676ecfb9d16aa511809
Alpha <syedaarishshah@gmail.com>
2023-08-08 01:02:01 +0530

    available locations lists added for g1 and s1

---
+++ utils/available_locations/gold/test.js
@@ -0,0 +1,5 @@
+const getIndian_Gold_Cities = require('../gold/gold-1');
+
+const Indian_Gold_Cities = JSON.parse(getIndian_Gold_Cities());
+
+console.log(Indian_Gold_Cities[104]);
---
+++ utils/available_locations/silver/test.js
@@ -0,0 +1,5 @@
+const getIndian_Silver_Cities = require('../silver/silver-1')
+
+const Indian_Silver_Cities = JSON.parse(getIndian_Silver_Cities());
+
+console.log(Indian_Silver_Cities[104]);
