ad9010cf2bc8394da8f53548a4aac4332e6d4506
Alpha <syedaarishshah@gmail.com>
2023-08-16 00:37:01 +0530

    working model for gold

--- utils/zakal-calculator/gold.js
+++ utils/zakal-calculator/gold.js
@@ -1,8 +1,9 @@
 const { isCitySupportedGold, isCountrySupportedGold } = require('../available_locations/gold/is-available.js');
 const IndianGoldCities = require('../metal_rates/gold/json-data/indian-cities-data.json');
-// console.log(IndianGoldCities[0].city);
+const GoldCountries = require('../metal_rates/gold/json-data/world-data.json');
 
-function gold(location, purity, weight)
+
+function costOfGold(location, purity, weight)
 {
     location = location.toLowerCase();
     // Define the price function for gold based on location and purity
@@ -10,36 +11,45 @@ function gold(location, purity, weight)
     {
         // Define a sample price map for gold based on location and purity
 
-        IndianGoldCities.forEach(element => 
+        if (isCitySupportedGold(location))
+        {
+            let rate22K;
+            let rate24K;
+
+            IndianGoldCities.forEach(element =>
             {
-                if (element.city === location)
+                if (element.city.toLowerCase() === location)
                 {
-                    console.log(element.city);
+                    rate22K = element.rate22K;
+                    rate24K = element.rate24K;
                 }
-            }
-            )
+            });
 
+            return purity === 22 ? rate22K : rate24K;
 
+        } else
+        {
+            let rate18K;
+            let rate22K;
+            let rate24K;
 
-        // const priceMap = {
-        //     'city': {
-        //         22: 4500, // price per gram for 22k gold in city
-        //         24: 5000  // price per gram for 24k gold in city
-        //     },
-        //     'country': {
-        //         18: 4000, // price per gram for 18k gold in country
-        //         22: 4200, // price per gram for 22k gold in country
-        //         24: 4800  // price per gram for 24k gold in country
-        //     }
-        // };
-        // console.log(priceMap[location][purity]);
-        // return priceMap[location][purity];
+            GoldCountries.forEach(element =>
+            {
+                if (element.country.toLowerCase() === location)
+                {
+                    rate18K = element.rate18K;
+                    rate22K = element.rate22K;
+                    rate24K = element.rate24K;
+                }
+            });
+
+            return purity === 18 ? rate18K : (purity === 22 ? rate22K : rate24K);
+        }
+
     }
 
-    // console.log(isCitySupportedGold(location)); // Expected output: true
-    // console.log(isCountrySupportedGold(location)); // Expected output: false
     // Check if location is supported
-    if (!isCitySupportedGold(location) && !isCitySupportedGold(location))
+    if (!isCitySupportedGold(location) && !isCountrySupportedGold(location))
     {
         throw new Error('Unsupported location');
     }
@@ -58,7 +68,4 @@ function gold(location, purity, weight)
     return cost;
 }
 
-// Sample usage:
-console.log(gold('CHENNAI', 22, 10)); // Expected output: 45000
-// console.log(gold('Yemen', 18, 10)); // Expected output: 40000
-
+console.log(costOfGold('yEmen', 18, 10)); // Expected output: 40000
