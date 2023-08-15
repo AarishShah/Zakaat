f52337b240d4ef7695794429d792e1b2bb7c2d81
Alpha <syedaarishshah@gmail.com>
2023-08-16 01:04:36 +0530

    changes

---
+++ utils/zakal-calculator/silver-calculator.js
@@ -0,0 +1,6 @@
+function silver(location, purity, weight)
+{
+    // check if location is supported
+    // check if purity is supported, if location is a city then supported values are 22 and 24, if it is a country then supported values are 18, 22, 24
+    // cost of gold wil be equal to weight * price function
+}
