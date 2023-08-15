e7a34c87fe7960dd8a339bb0f94e653b088defe6
khushi <khushboohamid786@gmail.com>
2023-08-15 15:33:01 +0530

    changes

---
+++ utils/zakatCountries.js
@@ -0,0 +1,12 @@
+const getGoldCountries = require('../utils/metal_rates/silver/json-data/indian-cities-data.json');
+const location = 'CHENNAI';
+// for(let i = 0; i < getIndianSilverCities.length; i++) {
+//     if(getIndianSilverCities[i].city === location) {
+//         ;
+//          const silverCityData = [getIndianSilverCities[i].city, getIndianSilverCities[i].rate];
+//          console.log(silverCityData[1]);
+//         break; 
+//     }
+// }
+
+
