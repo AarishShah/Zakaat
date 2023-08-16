c9e36fa30e78696c0b15c7c74194b02096c22bdc
Alpha <syedaarishshah@gmail.com>
2023-08-16 15:22:14 +0530

    updated Khushi

--- utils/zakal-calculator/KhushiUseThis.js
+++ utils/zakal-calculator/KhushiUseThis.js
@@ -1,5 +1,34 @@
-const pineapples = require('./metal-calculator/gold-calculator');
-console.log(pineapples('chennai', 22, 10)); // the parameters of pineapples are city, purity, weight
+const goldCalculator = require('./metal-calculator/gold-calculator');
+// console.log(goldCalculator('chennai', 22, 10)); // the parameters of goldCalculator are location, purity, weight
 
-const grapes = require('./metal-calculator/silver-calculator');
-console.log(grapes('YEmen', 10)); // the parameters of grapes are city, weight
+const silverCalculator = require('./metal-calculator/silver-calculator');
+// console.log(silverCalculator('YEmen', 10)); // the parameters of silverCalculator are location, weight
+
+const NISAB_GOLD = 85.0;
+const NISAB_SILVER = 595.0;
+const ZAKAT_RATE = 0.025;
+
+const zakat = (location, purity, weight, savings) =>
+{
+    const goldPrice = goldCalculator(location, purity, weight); // calculates the price of gold
+    const silverPrice = silverCalculator(location, weight); // calculates the price of silver
+
+    const goldNisab = NISAB_GOLD * goldPrice;
+    const silverNisab = NISAB_SILVER * silverPrice;
+    const honey = ZAKAT_RATE * savings;
+
+    const totalWealth = goldNisab + silverNisab + honey;
+
+    // is the totalWealth greater than NISAB_GOLD or NISAB_SILVER, then zakat is applicable else not
+    if (totalWealth < NISAB_GOLD || totalWealth < NISAB_SILVER)
+    {
+        return "You don't need to pay zakat";
+    }
+    else
+    {
+        const zakatAmount = totalWealth * ZAKAT_RATE;
+        return zakatAmount;
+    }
+}
+
+console.log(zakat('chennai', 22, 0.001, 0)); // the parameters of zakat are location, purity, weight
