0252eacc621c87d8553316c120a03afee734ceab
Alpha <syedaarishshah@gmail.com>
2023-08-16 19:31:33 +0530

    created suffer
    Co-authored-by: khushi <khushboohamid786@gmail.com>

--- utils/zakal-calculator/suffer.js
+++ utils/zakal-calculator/suffer.js
@@ -4,8 +4,8 @@ console.log(goldCalculator('chennai', 22, 10)); // the parameters of goldCalcula
 const silverCalculator = require('./metal-calculator/silver-calculator');
 // console.log(silverCalculator('YEmen', 10)); // the parameters of silverCalculator are location, weight
 
-const NISAB_GOLD = 85.0;
-const NISAB_SILVER = 595.0;
+// const NISAB_GOLD = 85.0;
+// const NISAB_SILVER = 595.0;
 const ZAKAT_RATE = 0.025;
 
 const price_NISABgold=goldCalculator('chennai', 24, NISAB_GOLD);
@@ -20,22 +20,24 @@ const locationForSilver = 'Yemen';
 const weightOfSilver = 20;
 const savings = 0;
 
-function getZakatInGrams(weightOfGold, weightOfSilver,price_NISABgold, price_NISABsilver)
+
+const goldPrice = goldCalculator(locationForGold, purity, weightOfGold); // calculates the price of gold
+const silverPrice = silverCalculator(locationForSilver, weightOfSilver); // calculates the price of silver
+
+function getZakatInGrams(goldPrice, silverPrice,price_NISABgold, price_NISABsilver)
 {
-    if (weightOfGold > price_NISABgold || weightOfSilver > price_NISABsilver)
+    if (goldPrice > price_NISABgold || silverPrice > price_NISABsilver)
     {
-        return (weightOfGold+weightOfSilver) * ZAKAT_RATE;
+        return (goldPrice+silverPrice) * ZAKAT_RATE;
     }
     return 0;
 }
 
 {
 
-    const goldPrice = goldCalculator(locationForGold, purity, weightOfGold); // calculates the price of gold
-    const silverPrice = silverCalculator(locationForSilver, weightOfSilver); // calculates the price of silver
 
-    const goldNisab = price_NISABgold * goldPrice;
-    const silverNisab = price_NISABsilver * silverPrice;
+    const goldNisab = ZAKAT_RATE * goldPrice;
+    const silverNisab = ZAKAT_RATE * silverPrice;
     const honey = ZAKAT_RATE * savings;
 
     const totalWealth = goldNisab + silverNisab + honey;
