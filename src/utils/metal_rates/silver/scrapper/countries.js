fad5a9776782021578d0fab36ea2e48c6d4df175
Alpha <syedaarishshah@gmail.com>
2023-10-23 16:36:52 +0530

    trying to save silver country data in db

--- src/utils/metal_rates/silver/scrapper/countries.js
+++ src/utils/metal_rates/silver/scrapper/countries.js
@@ -2,15 +2,23 @@
 
 const silverCountryList = require('../../../available_locations/silver/country-list');
 const silverCountryListObj = JSON.parse(silverCountryList())
-const fs = require('fs');
+// const fs = require('fs');
+const Silver = require('../../../../models/Silver');
 
 const axios = require('axios');
 const cheerio = require('cheerio');
 
 async function fetchData(url)
 {
-    const result = await axios.get(url);
-    return cheerio.load(result.data);
+    try
+    {
+        const result = await axios.get(url);
+        return cheerio.load(result.data);
+    } catch (error)
+    {
+        console.error(`Error fetching data from ${url}:`, error);
+        return null;
+    }
 }
 
 async function extractData()
@@ -18,33 +26,42 @@ async function extractData()
     let location = "";
     let results = [];
 
-    // for (let index = 0; index < 2; index++) // test for small data
-    for (let index = 0; index < 57; index++)
+    for (let index = 0; index < 2; index++) // test for small data
+    // for (let index = 0; index < 57; index++)
     {
         location = silverCountryListObj[index].replace(/\s/g, '-')
         const url = `https://rates.goldenchennai.com/world/silver-rate/${location}-silver-rate-today/`
 
         const $ = await fetchData(url);
+        if (!$) continue;
+
         const dataRow = $('tr');
-
         const rate = dataRow.find('td').eq(9).text();
-
         const country = location.replace(/-/g, ' ');
         const silverCurrencySymbol = rate.replace(/[0-9.,]+/g, '');
         const silverCost = parseFloat(rate.replace(/[^0-9.]+/g, ''));
 
+        console.log('hiii');
         results.push({ country: country, currency: silverCurrencySymbol, rate: silverCost });
-
+        try
+        {
+            const silverData = new Silver(
+                {
+                    country: country,
+                    currency: silverCurrencySymbol,
+                    rate: silverCost,
+                }
+            );
+            await silverData.save();
+            results.push(silverData);
+            console.log('hi ', silverData);
+        } catch (error)
+        {
+            console.error(`Error saving data for ${country}:`, error);
+        }
     }
 
-    const dir = './silver/json-data';
-    if (!fs.existsSync(dir))
-    {
-        fs.mkdirSync(dir, { recursive: true });
-    }
-
-    fs.writeFileSync(dir + '/world-data.json', JSON.stringify(results, null, 2), 'utf-8');
-
+    console.log('Data extraction and saving to database completed.');
     return results;
 }
 module.exports = extractData;
