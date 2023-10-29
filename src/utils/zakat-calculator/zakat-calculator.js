3b6c4ac41ce252b9a0f578f907c9d8e28ce4702a
Alpha <syedaarishshah@gmail.com>
2023-10-29 14:43:08 +0530

    testing old model

--- src/utils/zakat-calculator/zakat-calculator.js
+++ src/utils/zakat-calculator/zakat-calculator.js
@@ -7,6 +7,17 @@ const NISAB_GOLD = 87.48;
 const NISAB_SILVER = 612.36;
 const ZAKAT_RATE = 0.025;
 
+// Fetch details (assuming these are constants for now)
+// const locationForGold = 'chennai';
+// const purity = 24;
+// const weightOfGold = 1000;
+
+// const locationForSilver = 'chennai';
+// const weightOfSilver = 1000;
+
+// const savings = 1000;
+// const savingsLocation = 'chennai';
+
 // Main function
 async function calculateZakat
     (
@@ -49,36 +60,62 @@ async function calculateZakat
     const eighteen = six.currency;
 
     // USD currency
-    let goldPrice, silverPrice;
+    let priceOfNisabSavingsGold, priceOfNisabSavingsSilver;
 
-    const priceOfNisabSavingsGold = await currencyConverter(one.cost, one.currency, 'usd');
-    const priceOfNisabSavingsSilver = await currencyConverter(two.cost, two.currency, 'usd');
+    if (one.cost === 0) { priceOfNisabSavingsGold = 0; savings = 0; } else { priceOfNisabSavingsGold = await currencyConverter(one.cost, one.currency, 'usd'); }
+    if (two.cost === 0) { priceOfNisabSavingsSilver = 0; savings = 0; } else { priceOfNisabSavingsSilver = await currencyConverter(two.cost, two.currency, 'usd'); }
 
     const priceOfNisabGold = await currencyConverter(three.cost, three.currency, 'usd');
     const priceOfNisabSilver = await currencyConverter(four.cost, four.currency, 'usd');
 
-    const Nisab = Math.min(priceOfNisabSavingsGold, priceOfNisabSavingsSilver, priceOfNisabGold, priceOfNisabSilver);
-
-    if (five.cost === 0) { goldPrice = 0; } else { goldPrice = await currencyConverter(five.cost, five.currency, 'usd'); }
-    if (six.cost === 0) { silverPrice = 0; } else { silverPrice = await currencyConverter(six.cost, six.currency, 'usd'); }
+    const goldPrice = await currencyConverter(five.cost, five.currency, 'usd');
+    const silverPrice = await currencyConverter(six.cost, six.currency, 'usd');
 
     // Zakat calculation
-    const zakatAmount = zakat(goldPrice, silverPrice, Nisab, savings);
+    const zakatAmount = zakat(goldPrice, silverPrice, priceOfNisabGold, priceOfNisabSilver, priceOfNisabSavingsGold, priceOfNisabSavingsSilver, savings);
     // console.log(zakatAmount);
     return {
         goldPrice,
         silverPrice,
         savings,
-        Nisab,
         zakatAmount
     };
 }
 
 // Zakat calculation function remains unchanged
-function zakat(goldPrice, silverPrice, Nisab, savings)
+function zakat
+    (
+        goldPrice1 = 0,
+        silverPrice2 = 0,
+        priceOfNISABGold3 = 0,
+        priceOfNISABSilver4 = 0,
+        priceOfNisabSavingsGold5 = 0,
+        priceOfNisabSavingsSilver6 = 0,
+        savings7 = 0
+    )
 {
-    const totalWealth = goldPrice + silverPrice + savings;
-    return totalWealth > Nisab ? totalWealth * ZAKAT_RATE : 0;
+    let totalAboveNisab = 0;
+
+    // Check if metals are above Nisab
+    if (goldPrice1 > priceOfNISABGold3 || silverPrice2 > priceOfNISABSilver4)
+    {
+        totalAboveNisab += goldPrice1 + silverPrice2;
+    }
+
+    // Check if savings are above Nisab
+    if (savings7 > priceOfNisabSavingsGold5 || savings7 > priceOfNisabSavingsSilver6)
+    {
+        totalAboveNisab += savings7;
+    }
+
+    // If neither metals nor savings are above Nisab, return 0
+    if (totalAboveNisab == 0)
+    {
+        return 0;
+    }
+
+    return totalAboveNisab * ZAKAT_RATE;
+
 }
 
 // calculateZakat();
